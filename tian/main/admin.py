b3786b4a9e0df173852d569abf6aec51e68a94ef
Margarita Popova <margarita.kistanova@gmail.com>
2021-03-23 22:30:42 +0600

    Install ckeditor and register new Flatpages Admin

--- tian/main/admin.py
+++ tian/main/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+from ckeditor.widgets import CKEditorWidget
 
-# Register your models here.
+
+
+class FlatPageAdmin(FlatPageAdmin):
+    fieldsets = (
+        (None, {'fields': ('url', 'title', 'content', 'sites')}),
+        (_('Advanced options'), {
+            'classes': ('collapse',),
+            'fields': (
+                'enable_comments',
+                'registration_required',
+                'template_name',
+            ),
+        }),
+    )
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget},
+    }
+
+# Re-register FlatPageAdmin
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)

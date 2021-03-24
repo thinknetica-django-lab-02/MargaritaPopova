65b30508b983e6d8aa785995e7b5171afd6ba2ff
Margarita Popova <margarita.kistanova@gmail.com>
2021-03-24 13:32:07 +0600

    updated get_address method and added coordinates field validation

--- tian/main/models.py
+++ tian/main/models.py
@@ -1,20 +1,24 @@
-import json
-import requests
+from django.core.exceptions import ValidationError
 from django.db import models
-from tian.settings import GOOGLECLOUD_API
 
 
 class Building(models.Model):
     stories = models.SmallIntegerField(verbose_name='Этажность', null=True)
-    lng = models.FloatField(verbose_name='Долгота')
     lat = models.FloatField(verbose_name='Широта')
-    year = models.DateField(verbose_name='Год постройки')
+    lng = models.FloatField(verbose_name='Долгота')
+    year = models.SmallIntegerField(verbose_name='Год постройки')
+
+    def clean(self):
+        super().clean()
+        if not -90 <= self.lat <= 90 and -180 <= self.lng <= 180:
+            raise ValidationError('incorrect coordinates')
 
     def get_address_by_coordinates(self):
-        response = requests.get(
-           'https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key={}'.
-               format(self.lat, self.lng, GOOGLECLOUD_API)).json()
-        return json.loads(response)['formatted_address']
+        from geopy.geocoders import Nominatim
+
+        geolocator = Nominatim(user_agent="tian")
+        location = geolocator.reverse([self.lat, self.lng], timeout=10)
+        return location.address
 
     def __str__(self):
         return self.get_address_by_coordinates()

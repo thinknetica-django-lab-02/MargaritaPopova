d28e2e649dce5124d15d6d0ab134ebe103f96acd
Margarita Popova <margarita.kistanova@gmail.com>
2021-03-24 18:07:40 +0600

    Inlines, custom validators

--- tian/main/admin.py
+++ tian/main/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from main.models import Building, Apartment
 
-# Register your models here.
+
+class ApartmentInline(admin.TabularInline):
+    model = Apartment
+    extra = 1
+
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ('stories', 'year')}),
+        ('Координаты', {'fields': ('lng', 'lat',)}),
+    ]
+    inlines = [ApartmentInline]
+
+
+admin.site.register(Apartment)
--- tian/main/models.py
+++ tian/main/models.py
@@ -1,17 +1,30 @@
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 
-class Building(models.Model):
-    stories = models.SmallIntegerField(verbose_name='Этажность', null=True)
-    lat = models.FloatField(verbose_name='Широта')
-    lng = models.FloatField(verbose_name='Долгота')
-    year = models.SmallIntegerField(verbose_name='Год постройки')
+def validate_lat(lat):
+    if not -90 <= lat <= 90:
+        raise ValidationError(
+            _('%(lat)s must be between -90 and 90 degrees'),
+            params={'lat': lat},
+        )
 
-    def clean(self):
-        super().clean()
-        if not -90 <= self.lat <= 90 and -180 <= self.lng <= 180:
-            raise ValidationError('incorrect coordinates')
+
+def validate_lng(lng):
+    if not -180 <= lng <= 180:
+        raise ValidationError(
+            _('%(lng)s must be between -180 and 180 degrees'),
+            params={'lng': lng},
+        )
+
+
+class Building(models.Model):
+    stories = models.SmallIntegerField(verbose_name='Этажность', null=True, blank=True)
+    lat = models.FloatField(verbose_name='Широта', validators=[validate_lat])
+    lng = models.FloatField(verbose_name='Долгота', validators=[validate_lng])
+    year = models.SmallIntegerField(verbose_name='Год постройки', null=True, blank=True)
+
 
     def get_address_by_coordinates(self):
         from geopy.geocoders import Nominatim
@@ -32,7 +45,7 @@ class Apartment(models.Model):
     area = models.FloatField(verbose_name='Площадь')
 
     def __str__(self):
-        return self.building
+        return f'Квартира в {self.building.__str__()}'
 
 
 

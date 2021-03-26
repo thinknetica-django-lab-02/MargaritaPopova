646ff1d7b22102cef71bdd3f728d1ea4d9596089
Margarita Popova <margarita.kistanova@gmail.com>
2021-03-26 15:48:09 +0600

    models fixed a bit

--- tian/main/models.py
+++ tian/main/models.py
@@ -3,15 +3,6 @@ from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 
-with open('cities.txt', 'r') as f:
-    all_cities = list(f.read().split('\n'))
-
-
-CITIES_CHOICES = []
-for city in all_cities:
-    CITIES_CHOICES.append((city, city))
-
-
 def validate_lat(lat):
     if not -90 <= lat <= 90:
         raise ValidationError(
@@ -28,31 +19,41 @@ def validate_lng(lng):
         )
 
 
+class Location(models.Model):
+    name = models.CharField(verbose_name='Населенный пункт', blank=False, max_length=200)
+
+
+class Street(models.Model):
+    name = models.CharField(verbose_name='Улица', max_length=128)
+
+
 class Building(models.Model):
     stories = models.PositiveSmallIntegerField(verbose_name='Этажность', null=True, blank=True)
-    lat = models.FloatField(verbose_name='Широта', validators=[validate_lat], blank=True)
-    lng = models.FloatField(verbose_name='Долгота', validators=[validate_lng], blank=True)
-    city = models.CharField(verbose_name='Населенный пункт', choices=CITIES_CHOICES, default='', max_length=200)
-    street = models.CharField(verbose_name='Улица', default='', max_length=200)
-    number = models.PositiveSmallIntegerField(verbose_name='Дом', default=0)
+    lat = models.FloatField(verbose_name='Широта', validators=[validate_lat], blank=True, null=True)
+    lng = models.FloatField(verbose_name='Долгота', validators=[validate_lng], blank=True, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='buildings')
+    street = models.ForeignKey(Street, verbose_name='Улица', related_name='buildings', on_delete=models.CASCADE)
+    number = models.PositiveSmallIntegerField(verbose_name='Дом')
     year = models.PositiveSmallIntegerField(verbose_name='Год постройки', null=True, blank=True)
 
     def __str__(self):
-        if not (self.street or self.city):
+        if not (self.street or self.location):
             return f'Building {self.id}'
-        return f'{self.city}, ул. {self.street}, д. {self.number}'
+        return f'{self.location}, ул. {self.street}, д. {self.number}'
 
 
 class Apartment(models.Model):
     building = models.ForeignKey(Building, on_delete=models.CASCADE, verbose_name='Адрес')
-    story = models.SmallIntegerField(verbose_name='Этаж', null=True)
-    rooms = models.SmallIntegerField(verbose_name='Количество комнат', null=True)
+    story = models.PositiveSmallIntegerField(verbose_name='Этаж', null=True)
+    rooms = models.PositiveSmallIntegerField(verbose_name='Количество комнат', null=True)
     price = models.FloatField(verbose_name='Цена общая')
     area = models.FloatField(verbose_name='Площадь')
     number = models.PositiveSmallIntegerField(verbose_name='Номер квартиры')
 
     def __str__(self):
         return f'Квартира в {self.building.__str__()}'
+
+
 
 
 

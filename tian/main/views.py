a34ae917a3aa4eb03abcfd43982bd87908aad8cb
Margarita Popova <margarita.kistanova@gmail.com>
2021-03-28 22:51:52 +0300

    added username to template

--- tian/main/views.py
+++ tian/main/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {'turn_on_block': True}
+    context = {
+        'turn_on_block': True,
+    }
     return render(request, 'main/index.html', context)
